05dab1fd94576181dff3a785bb01432cc2926737
Adam McKee <adam.be.g84d@gmail.com>
2024-03-04 19:51:06 -0600

    fancy component reactivity

--- web/src/routes/(login)/login/+page.svelte
+++ web/src/routes/(login)/login/+page.svelte
@@ -1,9 +1,18 @@
+<script lang="ts">
+    let loginButtonEnabled = $state(false)
+    let emailInput: HTMLInputElement
+
+    function onInputKeyUp() {
+        loginButtonEnabled = /.+@.+/.test(emailInput.value)
+    }
+</script>
+
 <h1>Login!</h1>
 
 <form method="post">
     <label>
         Email
-        <input type="email" name="email"/>
+        <input type="email" name="email" bind:this={emailInput} onkeyup={onInputKeyUp}/>
     </label>
-    <button type="submit">Send login email</button>
+    <button type="submit" disabled={!loginButtonEnabled}>Send login email</button>
 </form>
