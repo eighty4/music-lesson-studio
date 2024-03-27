6b0261b8a957d9b43dba5ab1794091a761069988
Adam McKee <adam.be.g84d@gmail.com>
2024-03-27 14:51:54 -0500

    validate string type

--- web/src/lib/data/UserTypes.test.ts
+++ web/src/lib/data/UserTypes.test.ts
@@ -6,6 +6,7 @@ describe('UserTypes', () => {
         it('validates name', () => {
             expect(isValidName('abc')).toBeTruthy()
             expect(isValidName('ab')).toBeFalsy()
+            expect(isValidName(3 as any)).toBeFalsy()
             expect(isValidName(undefined as any)).toBeFalsy()
             expect(isValidName(null as any)).toBeFalsy()
         })
@@ -17,6 +18,7 @@ describe('UserTypes', () => {
             expect(isValidEmail('@eagleton.gov')).toBeFalsy()
             expect(isValidEmail('Television')).toBeFalsy()
             expect(isValidEmail('')).toBeFalsy()
+            expect(isValidEmail(3 as any)).toBeFalsy()
             expect(isValidEmail(undefined as any)).toBeFalsy()
             expect(isValidEmail(null as any)).toBeFalsy()
         })
--- web/src/lib/data/UserTypes.ts
+++ web/src/lib/data/UserTypes.ts
@@ -12,9 +12,20 @@ export interface User {
 }
 
 export function isValidName(name: string): boolean {
-    return !!name && name.length > 2
+    return isString(name) && name.length > 2
 }
 
 export function isValidEmail(email: string): boolean {
-    return /.+@.+/.test(email)
+    return isString(email) && /.+@.+/.test(email)
+}
+
+function isString(s: any): boolean {
+    switch (typeof s) {
+        case 'string':
+            return true
+        case 'object':
+            return s !== null && s.constructor.toString().startsWith('function String')
+        default:
+            return false
+    }
 }
