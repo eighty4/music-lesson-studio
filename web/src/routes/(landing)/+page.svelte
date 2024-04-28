acf4b0bbb69cd53a0f7268c3e099f3dc31c7abeb
Adam McKee <adam.be.g84d@gmail.com>
2024-04-28 17:05:28 -0500

    correct ui demo path

--- web/src/routes/(landing)/+page.svelte
+++ web/src/routes/(landing)/+page.svelte
@@ -7,4 +7,4 @@
 <p><a href="/signup">Create a school program</a></p>
 <p><a href="/lesson-plans">View lesson plans</a></p>
 <p><a href="/lesson-plans/new">Create a lesson plan</a></p>
-<p><a href="/ui">Demo the Studio UI</a></p>
+<p><a href="/studio">Demo the Studio UI</a></p>
