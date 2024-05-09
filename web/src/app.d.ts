a20dad5efae4dfb8effd69b74fc64156ec400581
Adam McKee <adam.be.g84d@gmail.com>
2024-05-09 17:21:54 -0500

    type check window global variables

--- web/src/app.d.ts
+++ web/src/app.d.ts
@@ -1,13 +1,20 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 declare global {
-	namespace App {
-		// interface Error {}
-		// interface Locals {}
-		// interface PageData {}
-		// interface PageState {}
-		// interface Platform {}
-	}
+    namespace App {
+        // interface Error {}
+        // interface Locals {}
+        // interface PageData {}
+        // interface PageState {}
+        // interface Platform {}
+    }
+    var mlsEditorSession: {
+        apiHost: string
+        planId?: string
+        planName?: string
+        unitId?: string
+    };
+    var serviceWorkerVersion: string | null;
 }
 
 export {};
