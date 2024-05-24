9d9f6463391a6cc108aa79d0a90ba5bc2b2fa45d
Adam McKee <adam.be.g84d@gmail.com>
2024-05-23 21:21:59 -0500

    ci check script

---
+++ ci_verify.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+set -e
+
+# run through all the checks done for ci
+
+cd packages/api_client
+echo '\n*** mls_api ***'
+dart format lib test --set-exit-if-changed
+flutter analyze
+flutter test
+cd ../..
+
+cd studio
+echo '\n*** studio ***'
+dart format lib test --set-exit-if-changed
+flutter analyze
+flutter test
+cd ..
+
+cd web
+echo '\n*** web ***'
+pnpm check
+pnpm test
+cd ..
+
+cd test
+echo '\n*** test ***'
+pnpm test
+cd ..
