3c4f1037d82b3eb7fe61f4b543459539c0675882
Adam McKee <adam.be.g84d@gmail.com>
2024-08-07 13:39:47 -0500

    natural like standardized convention

--- studio/lib/studio_editor.dart
+++ studio/lib/studio_editor.dart
@@ -127,9 +127,8 @@ class _StudioEditorState extends State<StudioEditor> {
               shortcuts: const <ShortcutActivator, Intent>{
                 SingleActivator(LogicalKeyboardKey.keyZ, meta: true):
                     UndoIntent(),
-                // todo is redo with `meta + shift + z` possible instead of `meta + y`
-                SingleActivator(LogicalKeyboardKey.keyY, meta: true):
-                    RedoIntent(),
+                SingleActivator(LogicalKeyboardKey.keyZ,
+                    meta: true, shift: true): RedoIntent(),
                 SingleActivator(LogicalKeyboardKey.escape): CancelIntent(),
                 SingleActivator(LogicalKeyboardKey.backspace): DeleteIntent(),
                 SingleActivator(LogicalKeyboardKey.delete): DeleteIntent(),
