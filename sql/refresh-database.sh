e53f7f41a7c0e3013193aed371c75e07c03f71e8
Adam McKee <adam.be.g84d@gmail.com>
2026-04-28 03:53:54 -0500

    sql refresh migrations script can be ran from any dir

--- sql/refresh-database.sh
+++ sql/refresh-database.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+cd "$(dirname "${BASH_SOURCE[0]}")" || exit 1
+
 echo "drop schema if exists music_lesson_studio cascade" | docker exec -i music-postgres psql -U eighty4
 < v001-init-schema.sql docker exec -i music-postgres psql -U eighty4
